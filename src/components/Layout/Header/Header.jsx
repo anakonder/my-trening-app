039ae325f7689bff732d3e11f7ac2b1aea61b210
anakonder <120507138+anakonder@users.noreply.github.com>
2023-11-11 01:56:02 +0200

    123

--- src/components/Layout/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import css from './Header.module.css'
-
-export const Header = ({hendleModadalLoginToggle}) => {
-    return (
-        <div className={css.header}>
-            <button className={css.loginBtn} type='button' onClick={hendleModadalLoginToggle}>Sign in</button>
-        </div>
-    )
-}
